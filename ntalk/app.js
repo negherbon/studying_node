2e71d80e895e5d7cf6148d3620aeb0e7a497887f
Rafael Negherbon <rafael.negherbon@gmail.com>
2015-08-26 17:22:19 -0300

    ntalk express with node with jade app

---
+++ ntalk/app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    load = require('express-load'),
+    routes = require('./routes');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+app.get('/usuarios', routes.user.index);
+
+load('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+
+app.listen(3000);
